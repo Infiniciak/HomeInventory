bd15fa9df78a01dcf20f9aeaae719f7faed39616
Bartoszw <infiniciak@gmail.com>
2026-02-07 22:42:39 +0100

    delete

--- app/src/main/java/pl/edu/ur/bw131534/homeinventory/HomeInventoryApplication.kt
+++
@@ -1,8 +0,0 @@
-package pl.edu.ur.bw131534.homeinventory
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class HomeInventoryApplication : Application() {
-}
